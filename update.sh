c96a297562c9c544193cd4fceebd2f845960ef64
redex2 <redex2@redex2.dev>
2023-05-06 12:39:13 +0200

    add auto update

---
+++ update.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+set -euo pipefail
+
+dir=`dirname -- "$0"`
+cd $dir
+
+wget -qO- https://sqlite.org/src/tarball/release/sqlite.tar.gz | tar xz
+
+mkdir sqlite/build
+cd sqlite/build
+
+../configure
+make sqlite3.c
+cd ../..
+
+anyDiff=0
+
+set +e
+diff sqlite3.c sqlite/build/sqlite3.c >> /dev/null
+if [ $? -ne 0 ]
+then
+	anyDiff=1
+	cp sqlite/build/sqlite3.c sqlite3.c
+fi
+diff sqlite3.h sqlite/build/sqlite3.h >> /dev/null
+if [ $? -ne 0 ]
+then
+	anyDiff=1
+	cp sqlite/build/sqlite3.h sqlite3.h
+fi
+set -e
+
+rm -r sqlite
+
+if [ $anyDiff -eq 1 ]
+then
+	cmake -B build || echo "failed build - aborted" && exit 0
+	rm -r build
+	git commit -am "update"
+	git push
+else
+	echo "up to date"
+fi
+
+echo "OK"
+exit 0
+
